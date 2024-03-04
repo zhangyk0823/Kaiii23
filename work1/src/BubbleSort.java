10db913aa34140a54079ee6bf70877e98597fe86
zhangyk0823 <161433878+zhangyk0823@users.noreply.github.com>
2024-03-04 20:05:56 +0800

    Add files via upload

---
+++ work1/src/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    //arrray =      {38,65,3,17,2}
+    public void bubbleSort(int [] array){
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j<array.length-1;j++ ){
+                if(array[j]>array[j+1]){  //65<3;{2,3,17,38,65}
+                    System.out.println("i value is"+i+"and j value is"+j);
+                    int temp =array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        for(int j=0; j<array.length;j++){
+             System.out.println(array[j]+"，");
+        }
+    }
+    
+}
---
+++ work1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int [] arrray = {38,65,3,17,2};
+        BubbleSort bubbleSort =  new BubbleSort();
+
+        bubbleSort.bubbleSort(arrray);
+    }
+}
